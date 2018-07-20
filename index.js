0b4c4f46e4783d6431a9c2ba06b7b9eb5822a0ee
Saju Sasidharan <Saju.Sasidharan-Cybage@domo.com>
2018-07-20 21:07:30 +0530

    Initials commits to build stack

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 /** @format */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
-console.log(App);
+if (__DEV__) {
+  import * from 'react-devtools';
+}
+
 AppRegistry.registerComponent(appName, () => App);
--- readme.md
+++ readme.md
@@ -4,3 +4,6 @@ https://s3.amazonaws.com/apollo-docs-1.x/react-native.html
 
 Apollo with Redux store;
 https://s3.amazonaws.com/apollo-docs-1.x/redux.html
+
+Debugging;
+https://medium.com/research-engineering-at-simply-technologies/react-native-debugging-like-a-god-9610ac2ffd12
